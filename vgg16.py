12f1423c38927710b5022e7afa789d8cb031c305
Fr-d-rik <frederikharder@gmail.com>
2017-05-09 16:36:15 +0200

    removed tensors as class variables - access by name is more

--- vgg16.py
+++ vgg16.py
@@ -2,9 +2,6 @@ import inspect
 import os
 import numpy as np
 import tensorflow as tf
-import time
-
-# VGG_MEAN = [103.939, 116.779, 123.68]
 
 
 class Vgg16:
@@ -21,69 +18,64 @@ class Vgg16:
 
     def build(self, rgb, rescale=255.0):
 
-        self.rgb_scaled = tf.multiply(rgb, rescale, name='rgb_scaled')
+        rgb_scaled = tf.multiply(rgb, rescale, name='rgb_scaled')
 
-        # Convert RGB to BGR
-        red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=self.rgb_scaled)
+        red, green, blue = tf.split(axis=3, num_or_size_splits=3, value=rgb_scaled)
 
-        self.bgr_normed = tf.concat(axis=3, values=[blue - self.imagenet_mean[2],
-                                                    green - self.imagenet_mean[1],
-                                                    red - self.imagenet_mean[0]],
-                                    name='bgr_normed')
+        bgr_normed = tf.concat(axis=3, values=[blue - self.imagenet_mean[2],
+                                               green - self.imagenet_mean[1],
+                                               red - self.imagenet_mean[0]],
+                               name='bgr_normed')
 
-        self.conv1_1, self.conv1_1_lin = self.conv_layer(self.bgr_normed, "conv1_1")
-        self.conv1_2, self.conv1_2_lin = self.conv_layer(self.conv1_1, "conv1_2")
-        self.pool1 = self.max_pool(self.conv1_2, 'pool1')
+        conv1_1 = self.conv_layer(bgr_normed, "conv1_1")
+        conv1_2 = self.conv_layer(conv1_1, "conv1_2")
+        pool1 = max_pool(conv1_2, 'pool1')
 
-        self.conv2_1, self.conv2_1_lin = self.conv_layer(self.pool1, "conv2_1")
-        self.conv2_2, self.conv2_2_lin = self.conv_layer(self.conv2_1, "conv2_2")
-        self.pool2 = self.max_pool(self.conv2_2, 'pool2')
+        conv2_1 = self.conv_layer(pool1, "conv2_1")
+        conv2_2 = self.conv_layer(conv2_1, "conv2_2")
+        pool2 = max_pool(conv2_2, 'pool2')
 
-        self.conv3_1, self.conv3_1_lin = self.conv_layer(self.pool2, "conv3_1")
-        self.conv3_2, self.conv3_2_lin = self.conv_layer(self.conv3_1, "conv3_2")
-        self.conv3_3, self.conv3_3_lin = self.conv_layer(self.conv3_2, "conv3_3")
-        self.pool3 = self.max_pool(self.conv3_3, 'pool3')
+        conv3_1 = self.conv_layer(pool2, "conv3_1")
+        conv3_2 = self.conv_layer(conv3_1, "conv3_2")
+        conv3_3 = self.conv_layer(conv3_2, "conv3_3")
+        pool3 = max_pool(conv3_3, 'pool3')
 
-        self.conv4_1, self.conv4_1_lin = self.conv_layer(self.pool3, "conv4_1")
-        self.conv4_2, self.conv4_2_lin = self.conv_layer(self.conv4_1, "conv4_2")
-        self.conv4_3, self.conv4_3_lin = self.conv_layer(self.conv4_2, "conv4_3")
-        self.pool4 = self.max_pool(self.conv4_3, 'pool4')
+        conv4_1 = self.conv_layer(pool3, "conv4_1")
+        conv4_2 = self.conv_layer(conv4_1, "conv4_2")
+        conv4_3 = self.conv_layer(conv4_2, "conv4_3")
+        pool4 = max_pool(conv4_3, 'pool4')
 
-        self.conv5_1, self.conv5_1_lin = self.conv_layer(self.pool4, "conv5_1")
-        self.conv5_2, self.conv5_2_lin = self.conv_layer(self.conv5_1, "conv5_2")
-        self.conv5_3, self.conv5_3_lin = self.conv_layer(self.conv5_2, "conv5_3")
-        self.pool5 = self.max_pool(self.conv5_3, 'pool5')
+        conv5_1 = self.conv_layer(pool4, "conv5_1")
+        conv5_2 = self.conv_layer(conv5_1, "conv5_2")
+        conv5_3 = self.conv_layer(conv5_2, "conv5_3")
+        pool5 = max_pool(conv5_3, 'pool5')
 
-        self.fc6 = self.fc_layer(self.pool5, "fc6")
-        assert self.fc6.get_shape().as_list()[1:] == [4096]
-        self.relu6 = tf.nn.relu(self.fc6)
+        fc6 = self.fc_layer(pool5, "fc6")
+        assert fc6.get_shape().as_list()[1:] == [4096]
+        relu6 = tf.nn.relu(fc6)
 
-        self.fc7 = self.fc_layer(self.relu6, "fc7")
-        self.relu7 = tf.nn.relu(self.fc7)
+        fc7 = self.fc_layer(relu6, "fc7")
+        relu7 = tf.nn.relu(fc7)
 
-        self.fc8 = self.fc_layer(self.relu7, "fc8")
+        fc8 = self.fc_layer(relu7, "fc8")
 
-        self.prob = tf.nn.softmax(self.fc8, name="prob")
+        tf.nn.softmax(fc8, name="prob")
 
         self.data_dict = None
 
-    def avg_pool(self, bottom, name):
-        return tf.nn.avg_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
-
-    def max_pool(self, bottom, name):
-        return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
-
+    # noinspection PyUnresolvedReferences
     def conv_layer(self, bottom, name):
         with tf.variable_scope(name):
-            filt = self.get_conv_filter(name)
+            filt = tf.constant(self.data_dict[name][0], name="filter")
 
             conv = tf.nn.conv2d(bottom, filt, [1, 1, 1, 1], padding='SAME')
 
-            conv_biases = self.get_bias(name)
+            conv_biases = tf.constant(self.data_dict[name][1], name="biases")
             conv_lin = tf.nn.bias_add(conv, conv_biases, name='lin')
             conv = tf.nn.relu(conv_lin, name='relu')
-            return conv, conv_lin
+            return conv
 
+    # noinspection PyUnresolvedReferences
     def fc_layer(self, bottom, name):
         with tf.variable_scope(name):
             shape = bottom.get_shape().as_list()
@@ -92,20 +84,13 @@ class Vgg16:
                 dim *= d
             x = tf.reshape(bottom, [-1, dim])
 
-            weights = self.get_fc_weight(name)
-            biases = self.get_bias(name)
+            weights = tf.constant(self.data_dict[name][0], name="weights")
+            biases = tf.constant(self.data_dict[name][1], name="biases")
 
-            # Fully connected layer. Note that the '+' operation automatically
-            # broadcasts the biases.
             fc = tf.nn.bias_add(tf.matmul(x, weights), biases)
 
             return fc
 
-    def get_conv_filter(self, name):
-        return tf.constant(self.data_dict[name][0], name="filter")
 
-    def get_bias(self, name):
-        return tf.constant(self.data_dict[name][1], name="biases")
-
-    def get_fc_weight(self, name):
-        return tf.constant(self.data_dict[name][0], name="weights")
+def max_pool(bottom, name):
+    return tf.nn.max_pool(bottom, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME', name=name)
